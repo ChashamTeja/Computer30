2d9221673c1d84d6d846b68b95e7c69d1d4bc1ab
1100023209 <1100023209@L815WD220013.edu.cbe.ab.ca>
2022-04-19 10:26:33 -0600

    ZX

--- Chapter11/src/findandreplace/findandreplace.java
+++ Chapter11/src/findandreplace/findandreplace.java
@@ -8,34 +8,49 @@ import java.util.Scanner;
 public class findandreplace {
     public static void main(String[] args) {
         
+    	//promote the user to enter the word they want to replace 
     	Scanner input = new Scanner(System.in);
         System.out.println("What is the search word?");
        
+        
+        
+        //promote the user to enter the new word
         String search = input.nextLine();
         System.out.println("What is the replacement word?");
+      
        
+        //promote the user to enter the file location
         String replacementWord = input.nextLine();
         System.out.println("What is the source file?");
-       
+     
+        
+        //promote the user to enter the name of new file
         String source = input.nextLine();
         System.out.println("What is the destination file?");
        
         String destination = input.nextLine();
         input.close();
+        
+        // find file 
         try {
             FileWriter fileOut = new FileWriter(destination);
             BufferedWriter bw = new BufferedWriter(fileOut);
             Scanner fileRead = new Scanner(new File(source));
+           
+            //find the word
+            
             while (fileRead.hasNextLine()) {
                 String data = fileRead.nextLine();
                 bw.write(data.replace(search, replacementWord));
                 bw.newLine();
             }
+            //replace data 
             bw.flush();
             bw.close();
             fileRead.close();
             System.out.println("Data replaced successfully.");
         } 
+        // show errors if file is not found 
         catch (IOException e) {
             System.out.println(e.getMessage());
         }
