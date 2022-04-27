2aa6f3417d552802bb2fa31b14e2141eec9f4066
1100023209 <1100023209@L815WD220013.edu.cbe.ab.ca>
2022-04-27 11:31:01 -0600

    sads

--- Chapter11/src/roster/StuName.java
+++ Chapter11/src/roster/StuName.java
@@ -25,43 +25,39 @@ public class StuName implements Serializable {
 	}
 
 
-	/**
-	 * @param firstName
-	 * @param lastName
-	 */
+	
 	public StuName(String firstName, String lastName) {
 		this.firstName = firstName;
 		this.lastName = lastName;
 	}
 
 
-	/**
-	 * @return the firstName
-	 */
+	
+	 // return the firstName
+	 
 	public String getFirstName() {
 		return firstName;
 	}
 
 
-	/**
-	 * @param firstName the firstName to set
-	 */
+	
+	 // set firstname
+	 
 	public void setFirstName(String firstName) {
 		this.firstName = firstName;
 	}
 
 
-	/**
-	 * @return the lastName
-	 */
+	
+	 //return the lastName
+	 
 	public String getLastName() {
 		return lastName;
 	}
 
 
-	/**
-	 * @param lastName the lastName to set
-	 */
+	// lastName to set
+	
 	public void setLastName(String lastName) {
 		this.lastName = lastName;
 	}
